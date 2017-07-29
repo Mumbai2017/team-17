f1674836e5093aa048250773b0ba4c027532684e
Suraj-Krishnan <surajkrishnan1995@gmail.com>
2017-07-29 22:26:29 +0530

    Android commit

---
+++ FinalMyApplication/app/src/main/java/com/example/admin/finalmyapplication/PatientWishActivity.java
@@ -0,0 +1,13 @@
+package com.example.admin.finalmyapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PatientWishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_wish);
+    }
+}
